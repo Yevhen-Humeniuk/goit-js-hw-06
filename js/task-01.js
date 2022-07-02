1b0ec079ba96c9b964d108e1efe63527c96c824e
Yevhen-Humeniuk <gumenyukevgen2@gmail.com>
2022-07-02 22:01:55 +0300

    start-HW
    start creat code for HW6

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const list = document.querySelector("#categories");
+console.log("list", list);
+
+
+const findLengdth = document.querySelector("#categories");
+console.log(findLengdth.children.length);
+
+
+const findCategory = document.querySelector("#categories");
+console.log(findCategory.elem.firstChild);
---
+++ js/task-02.js
@@ -0,0 +1,16 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const listIngridient = document.querySelector("#ingredients");
+  const addModLi = ingredients.map(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    li.classList.add("item");
+    return li;
+    });
+  listIngridient.append(...addModLi);

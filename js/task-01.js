455440556358180f3f4b685523b61cfa50d3bb0f
Yevhen-Humeniuk <gumenyukevgen2@gmail.com>
2022-07-02 23:11:33 +0300

    finish-first-task

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,15 @@ console.log("list", list);
 
 
 const findLengdth = document.querySelector("#categories");
-console.log(findLengdth.children.length);
+console.log(`Number of categories: ${findLengdth.children.length}`);
 
 
-const findCategory = document.querySelector("#categories");
-console.log(findCategory.elem.firstChild);
+const findCategories = document.querySelectorAll(".item");
+
+
+for (const findCategory of findCategories) {
+    const categoryName = findCategory.querySelector("h2");
+    const categoryLengdth = findCategory.querySelector("ul")
+    console.log(`Category: ${categoryName.textContent}, Elements: ${categoryLengdth.children.length}`);
+}
+
